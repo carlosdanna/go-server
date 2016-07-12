8dd28c788f4386a2222e3971b2f5bc5347b21309
Carlos Danna <CARLOS.DANNA@asurion.com>
2016-07-12 14:10:47 -0400

    Connecting my basic service with a mongo database in mlab

--- server.go
+++ server.go
@@ -6,9 +6,18 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var employees []Employee = createTestEmployee()
+
+type emp struct {
+	Firstname string
+	Lastname  string
+	Age       int
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
@@ -29,10 +38,55 @@ func GetEmployeesx(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(format))
 }
 
+func GetEmployeesDB(w http.ResponseWriter, r *http.Request) {
+	session, err := mgo.Dial("mongodb://admin:password@ds027809.mlab.com:27809/go-connect")
+	if err == nil {
+		c := session.DB("go-connect")
+		fmt.Println(c, "connected")
+	} else {
+		fmt.Println("Error: ", err)
+	}
+	var e []emp
+	c := session.DB("go-connect").C("Employee")
+
+	query := c.Find(bson.M{}).All(&e)
+	session.DB("go-connect").C("Employee").Find(query)
+	fmt.Println(e)
+}
+
+func PostEmployee(w http.ResponseWriter, r *http.Request) {
+	var e emp
+	// buff, _ := ioutil.ReadAll(r.Body)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&e)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Print(string(buff))
+	fmt.Println(e)
+	format, _ := json.Marshal(e)
+	fmt.Println(string(format))
+	session, err := mgo.Dial("mongodb://admin:password@ds027809.mlab.com:27809/go-connect")
+	if err == nil {
+		c := session.DB("go-connect")
+		fmt.Println(c, "connected")
+	} else {
+		fmt.Println("Error: ", err)
+	}
+	errInsert := session.DB("go-connect").C("Employee").Insert(e)
+	if errInsert != nil {
+		panic(err)
+	}
+	session.Close()
+
+}
+
 func main() {
 
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/getEmployeesj", GetEmployeesj)
 	http.HandleFunc("/getEmployeesx", GetEmployeesx)
+	http.HandleFunc("/postEmployee", PostEmployee)
+	http.HandleFunc("/getEmployeesDb", GetEmployeesDB)
 	http.ListenAndServe(":8000", nil)
 }
